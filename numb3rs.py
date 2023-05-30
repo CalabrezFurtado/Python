7324316383ee375ee56eefd63dfb0f591fea18a2
CalabrezFurtado <calabrez.adv@gmail.com>
2023-05-30 14:01:07 -0300

    Add files via upload

---
+++ numb3rs.py
@@ -0,0 +1,14 @@
+import re
+import sys
+
+
+def main():
+    print(validate(input("IPv4 Address: ")))
+
+
+def validate(ip):
+
+    return bool(re.search (r"^((([0-2]?[0-5]?[0-5])|([0-2]?[0-4]?[0-9])|([0-1]?[0-9]?[0-9]))(\.|$)){4}", ip))
+
+if __name__ == "__main__":
+    main()
---
+++ test_numb3rs.py
@@ -0,0 +1,12 @@
+from numb3rs import validate
+
+def test_validate_numbers():
+
+    assert validate("255.255.255.255") == True
+    assert validate("25.5.355.255.25.55") == False
+    assert validate("255.355.255.255") == False
+
+def test_validate_strorelse():
+
+    assert validate("aaaaa") == False
+    assert validate("aaaa__*(*&a") == False
